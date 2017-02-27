177b4189cfcb3d4774dfcfce84a38ccfc77d8adb
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-02-27 13:38:51 +0300

    cleanup

--- dist/bundle.js
+++ dist/bundle.js
@@ -3747,6 +3747,8 @@ var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = [
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
+/* eslint-disable import/prefer-default-export */
+
 var getDomain = exports.getDomain = function getDomain(keys, items) {
   var getValues = function getValues(item) {
     return keys.reduce(function (acc, key) {
