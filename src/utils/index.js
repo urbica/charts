d99980ae4cacbc8c5e22973b4789aa633ef37e3d
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-02-27 13:36:58 +0300

    build with webpack

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
 export const getDomain = (keys, items) => {
   const getValues = item =>
     keys.reduce((acc, key) => {
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: '@urbica/components',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
